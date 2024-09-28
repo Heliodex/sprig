91336c4ac6cc0a4ec8aa4c36c76ff0da5eae5224
Lewin Kelly <heli@odex.cf>
2024-09-28 23:50:54 +0100

    Turn it file into sprig-compatible music

--- music/main.go
+++ music/main.go
@@ -2,36 +2,81 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/gotracker/playback/format/it"
 	"github.com/gotracker/playback/format/it/layout"
+	"github.com/gotracker/playback/period"
 	"github.com/gotracker/playback/format/it/volume"
 	"github.com/gotracker/playback/index"
-	"github.com/gotracker/playback/period"
 	"github.com/gotracker/playback/player/feature"
 	"github.com/gotracker/playback/song"
 )
 
+var instruments = []rune{'~', '^', '-', '/'}
+
 func main() {
 	data, err := it.IT.Load("./track.it", []feature.Feature{})
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
 
-	pattern, err := data.GetPattern(0)
+	bpm := float64(data.GetInitialBPM())
+	ms := int(bpm / 0.6 / 2)
+
+	patterns := []index.Pattern{0}
+
+	var toPrint strings.Builder
+
+	for _, p := range patterns {
+		pattern, err := data.GetPattern(p)
+		if err != nil {
+			panic(err)
+		}
+
+		rows := pattern.NumRows()
+		for i := range index.Row(rows) {
+			row := pattern.GetRow(i).(layout.Row[period.Linear])
+
+			toPrint.WriteString(fmt.Sprintf("\n%d", ms))
+
+			firstNote := true
+
+			row.ForEach(func(c index.Channel, d song.ChannelData[volume.Volume]) (bool, error) {
+				n := d.GetNote()
+				ns := n.String()
+				if ns == "C-0" {
+					return c < 4, nil
+				}
+
+				note := strings.ReplaceAll(ns, "-", "")
+				instrument := instruments[c]
+
+				if firstNote {
+					toPrint.WriteString(": ")
+				} else {
+					toPrint.WriteString(" + ")
+				}
+				firstNote = false
+
+				toPrint.WriteString(fmt.Sprintf("%s%c%d", note, instrument, ms))
+
+				return c < 4, nil // 5 channelz
+			})
+
+			toPrint.WriteString(",")
+		}
+	}
+
+	// write to file
+	f, err := os.Create("output.txt")
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
-
-	row := pattern.GetRow(0).(layout.Row[period.Linear])
-
-	row.ForEach(func(c index.Channel, d song.ChannelData[volume.Volume]) (bool, error) {
-		note := d.GetNote()
-
-		fmt.Printf("Channel %d: Note %s\n", c, note)
-
-		return c < 4, nil // 5 channel
-	})
+	defer f.Close()
+	_, err = f.WriteString(toPrint.String())
+	if err != nil {
+		panic(err)
+	}
 }
