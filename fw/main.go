6555ab03da782d276153fc93b5c98b88d37bb3c6
Lewin Kelly <heli@odex.cf>
2025-06-07 03:20:00 +0100

    Text drawing as a display benchmark

--- fw/main.go
+++ fw/main.go
@@ -21,6 +21,35 @@ const (
 	height = 128
 )
 
+// nothing cares about the opacity anyway
+type RGB struct {
+	R, G, B uint8
+}
+
+func (c RGB) RGBA() color.RGBA {
+	return color.RGBA{c.R, c.G, c.B, 0xff}
+}
+
+func Greyscale(v uint8) RGB {
+	return RGB{v, v, v}
+}
+
+func FromRGBA(c color.RGBA) RGB {
+	return RGB{
+		uint8(uint16(c.R) * uint16(c.A) / 0xff),
+		uint8(uint16(c.G) * uint16(c.A) / 0xff),
+		uint8(uint16(c.B) * uint16(c.A) / 0xff),
+	}
+}
+
+var (
+	black = RGB{0x00, 0x00, 0x00}
+	white = RGB{0xff, 0xff, 0xff}
+	red   = RGB{0xff, 0x00, 0x00}
+	green = RGB{0x00, 0xff, 0x00}
+	blue  = RGB{0x00, 0x00, 0xff}
+)
+
 func main() {
 	// display things
 	machine.SPI0.Configure(machine.SPIConfig{
@@ -59,7 +88,7 @@ func main() {
 		d.SetPixel(int16(x), int16(y), c)
 	}
 
-	drawText := func(text string, xPos, yPos uint8) {
+	drawText := func(text string, xPos, yPos uint8, colour RGB) {
 		chars := textToChars(text)
 
 		for _, char := range chars {
@@ -70,12 +99,14 @@ func main() {
 
 			for y, row := range char.content {
 				for x, b := range row {
-					draw(xPos+uint8(x), yPos+uint8(y), color.RGBA{
-						R: b,
-						G: b,
-						B: b,
-						A: 0xff,
-					})
+					if b == 0 {
+						continue // skip empty pixels
+					}
+
+					colour := colour.RGBA()
+					colour.A = b
+
+					draw(xPos+uint8(x), yPos+uint8(y), colour)
 				}
 			}
 
@@ -83,10 +114,25 @@ func main() {
 		}
 	}
 
-	drawText("Hello, Sprig!", 2, 2)
+	fillScreen := func(c RGB) {
+		d.FillScreen(c.RGBA())
+	}
+
+	const txt = "Sprig text drawing"
+
+	for {
+		const n = 60
+
+		for i := range n {
+			drawText(txt, 0, uint8(i), Greyscale(uint8(i*0xff/n)))
+		}
+		for i := range n {
+			drawText(txt, 0, uint8(i), Greyscale(0xff-uint8(i*0xff/n)))
+		}
+	}
 
 	setLeft(0)
 	setRight(0)
-	// d.FillScreen(color.RGBA{0x00, 0x00, 0x00, 0xff})
+	fillScreen(black)
 	// d.EnableBacklight(false)
 }
