992f901a5bf9d169b0160a2eb7cd161f900c1ac4
Lewin Kelly <heli@odex.cf>
2025-10-15 12:45:53 +0100

    3D spinning cube rendering

--- fw/game/game.go
+++ fw/game/game.go
@@ -78,6 +78,102 @@ func (s *SineWave) drawTo(buf *util.ScreenBuffer) {
 	}
 }
 
+type Cube3D struct {
+	colour               util.Pixel
+	pos                  util.Vector2
+	size, angle1, angle2 int
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func drawLine(buf *util.ScreenBuffer, p1, p2 util.Vector2, colour util.Pixel) {
+	dx := abs(p2.X - p1.X)
+	dy := -abs(p2.Y - p1.Y)
+	sx := -1
+	if p1.X < p2.X {
+		sx = 1
+	}
+	sy := -1
+	if p1.Y < p2.Y {
+		sy = 1
+	}
+	e := dx + dy
+
+	for {
+		buf.Set(p1.X, p1.Y, colour)
+		if p1.X == p2.X && p1.Y == p2.Y {
+			break
+		}
+		e2 := 2 * e
+		if e2 >= dy {
+			e += dy
+			p1.X += sx
+		}
+		if e2 <= dx {
+			e += dx
+			p1.Y += sy
+		}
+	}
+}
+
+func (c *Cube3D) drawTo(buf *util.ScreenBuffer) {
+	// rotating cube in integer 3D
+	angle1 := float64(c.angle1) / 10
+	angle2 := float64(c.angle2) / 10
+	sin1, cos1 := math.Sin(angle1), math.Cos(angle1)
+	sin2, cos2 := math.Sin(angle2), math.Cos(angle2)
+
+	// 8 corners of the cube
+	points := [8]util.Vector2{}
+	for i := range points {
+		x := float64((i>>0)&1*2-1) * float64(c.size)
+		y := float64((i>>1)&1*2-1) * float64(c.size)
+		z := float64((i>>2)&1*2-1) * float64(c.size)
+
+		// rotate around Y axis
+		xz := x*cos1 - z*sin1
+		z = x*sin1 + z*cos1
+		x = xz
+
+		// rotate around X axis
+		yz := y*cos2 - z*sin2
+		z = y*sin2 + z*cos2
+		y = yz
+
+		// project 3D to 2D (simple orthographic projection)
+		f := 20.0 / (z + 40) // perspective factor
+		sx := int(x*f) + c.pos.X
+		sy := int(y*f) + c.pos.Y
+
+		points[i] = util.Vector2{X: sx, Y: sy}
+	}
+
+	// draw edges
+	edges := [][2]int{
+		{0, 1},
+		{1, 3},
+		{3, 2},
+		{2, 0},
+		{4, 5},
+		{5, 7},
+		{7, 6},
+		{6, 4},
+		{0, 4},
+		{1, 5},
+		{2, 6},
+		{3, 7},
+	}
+
+	for _, e := range edges {
+		drawLine(buf, points[e[0]], points[e[1]], c.colour)
+	}
+}
+
 // intro to show, in the event that something else is loading or to show information
 func Splash(en util.Engine) {
 	freq := en.CPUFrequency()
@@ -168,11 +264,19 @@ func (s *State) Update(en util.Engine) {
 		phase:      s.f,
 	}
 
+	cube := &Cube3D{
+		colour: util.Yellow,
+		pos:    util.Vector2{X: util.Width / 2, Y: util.Height / 2},
+		size:   18,
+		angle1: s.f,
+		angle2: s.f / 2,
+	}
+
 	ui := []UIElement{grid, x, y}
 	for _, t := range Texts {
 		ui = append(ui, t)
 	}
-	ui = append(ui, sine)
+	ui = append(ui, sine, cube)
 
 	for _, e := range ui {
 		e.drawTo(en.ScreenBuffer())
